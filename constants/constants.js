8ec4218d7e6eaa448eac565e3bac00480abd3b4d
Andrew Qiao <andrewqiao2004@gmail.com>
2022-12-18 18:09:05 -0500

    Update about text

--- constants/constants.js
+++ constants/constants.js
@@ -311,7 +311,7 @@ export const EXPERIENCES = [
 ];
 
 export const ABOUT = [
-  "Hi! I'm Andrew, a first year CS student at the University of Waterloo.",
+  "Hi! I'm Andrew, a CS student at the University of Waterloo.",
   "Currently, I'm a Software Engineer at AVIEW International, leading development on the startup's website and using portal using React.js and AWS.",
   "Additionally, I'm a Research Intern at the University of Toronto, studying the utility and perception of AI in the workplace through building applications embedded with AI models such as GPT-3.",
   'Some of the things I like to do in my free time include playing tennis, searching for design inspiration, and solving Leetcode problems.',
