33b73935f2c883398d9f3d35bfccd65f531f2576
Andrew Qiao <andrewqiao2004@gmail.com>
2023-10-13 21:09:13 -0400

    update about;

--- constants/about.ts
+++ constants/about.ts
@@ -1,7 +1,6 @@
 export const ABOUT = [
-  "Hi! I'm Andrew, a CS student at the University of Waterloo.",
-  "Currently, I'm a Portfolio Analytics Developer at TD Bank, where I implement financial tools and data pipelines to help our firm's Portfolio Managers in managing $421B+ in client assets.",
-  "Additionally, as a Co-Founder of a stealth startup, we're reimagining the way healthcare providers interact with their patients through an innovative data-driven application.",
-  'Some of the things I like to do in my free time include playing tennis, going to the gym, and experimenting with spatial computing.',
-  "That said, I'd love to get to know you. Feel free to message me about new opportunities or if you'd like to have a conversation!",
+  "👋 Hi! I'm Andrew, a Computer Science student @ UWaterloo. Currently, I'm interning as a Software Developer @ University Health Network, where I'm integrating fine-tuned AI models into full-stack platforms, driving AI adoption in Canadian healthcare.",
+  "In the past, I've also interned at TD Bank, developing financial data pipelines and web applications. I also worked at the University of Toronto on a university-partnered research study with OpenAI, exploring the disruptions and utilities of AI in the workforce.",
+  "In my free time, I love to play tennis, lift weights, and dabble in UI/UX design. I'm always open to meeting new people, so if you're building something interesting or want to chat, feel free to send me a message!",
+  "I'm also currently seeking Summer 2024 internships. If you think I'd be a match for your team, I'd love to hear from you!",
 ];
